166d823386a60af3e78fc3a108d243981b1af0ff
unknown <471151015@qq.com>
2018-12-20 21:08:07 +0800

    18/12/20

---
+++ Scikit-learn/01.py
@@ -0,0 +1,24 @@
+from sklearn.feature_extraction import DictVectorizer
+
+
+def dictvec():
+    """
+    字典数据收起
+    :return:none
+    """
+    # 实例化
+    dict = DictVectorizer(sparse=False)
+    # 调用fit_transform
+    data = dict.fit_transform([
+            {'name': 'liu', 'sex': 'man'},
+            {'name': 'feng', 'sex': 'woman'},
+            {'name': 'zhao', 'sex': 'woman'},
+        ])
+    # 展示特征类别
+    print(dict.get_feature_names())
+    # 将字典数据转化成One-hot编码，数字数据不需要转化
+    print(data)
+
+
+if __name__ == '__main__':
+    dictvec()
